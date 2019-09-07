1e2f1d10e49b5c714e619d95ecdd209c041bd193
mifune96 <imrank.sensea@gmail.com>
2019-09-07 14:52:26 +0700

    Add file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const PORT = process.env.PORT || 3000;
+
+app.set('views' , path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.render('index.ejs');
+});
+
+app.get('/about', function(req, res){
+    res.send('Page About Me !');
+});
+
+app.listen(PORT);
+console.log('Server running on Port '+PORT);
