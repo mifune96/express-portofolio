995e2a2ca93ed95d73b9a38d93d085d35a93eeb4
mifune96 <imrank.sensea@gmail.com>
2019-09-07 16:07:13 +0700

    hapus

--- public/js/data.js
+++ public/js/data.js
@@ -1,7 +1,7 @@
 const data = {
     nav: {
         title: {
-            text: `Louis Iaeger V`,
+            text: `Ali Imran`,
             link: `#app`
         },
         navLinks: [
@@ -24,157 +24,57 @@ const data = {
         ]
     },
     jumbo: {
-        text: `Hi, I'm Louis. I design & build websites.`,
+        text: `Hi, I'm Ali. I design & build android app.`,
         buttonText: `Yes, I'm available for hire.`,
         buttonLink: `#contact`,
         scrollDownLink: '#about',
         img: `img/desk4.jpg`
     },
     about: {
-        img: `img/me.jpg`,
-        aboutText: `Passionate programmer pursuing a career path in web/app development and mixed reality technologies. I bring a strong background in UX and attention to detail that will increase engagement between your company and its target audience.`
+        img: `img/ali.jpeg`,
+        aboutText: `Passionate programmer pursuing a career path in android/app development and mixed reality technologies.`
     },
     portfolio: {
         backgroundImg: `img/zipper.png`,
         projects: [
         {
-            title:          `Breakout`,
-            description:    `Breakout arcade game made w/PhaserJS Framework`,
+            title:          `Fabric Store Mobile`,
+            description:    `FAbric Store Mobile merupakan aplikasi penjualan kain dan masih dalam tahap pengembangan`,
             img:            `https://github.com/louisiaegerv/Project-Images/blob/master/breakout.png?raw=true`,
-            link:           `https://louisiaegerv.github.io/breakout/`,
+            link:           `https://github.com/mifune96/FabricMobile/`,
             tags:           [
-                `JavaScript`, `Game Dev`
+                `Java`, `Android Dev` , `Restfull API`
             ]
         },
         {
-            title:          `LouTube`,
-            description:    `Simple YouTube Clone made with React`,
+            title:          `UTICommunuty`,
+            description:    `Simpel aplikasi chat dengan firebase`,
             img:            `https://github.com/louisiaegerv/Project-Images/blob/master/loutube-wide.png?raw=true`,
-            link:           `https://www.youtube.com/watch?v=nr0FQeXYmws&feature=youtu.be`,
+            link:           `https://github.com/mifune96/UTICommunity`,
             tags:           [
-                `JavaScript`, `React`
-            ]
-        },
-        {
-            title:          `guestlist`,
-            description:    `Manage your party's RSVP's with this responsive web app made with JavaScript!`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/GuestListApp.jpg?raw=true`,
-            link:           `https://louisiaegerv.github.io/guestlist/`,
-            tags:           [
-                `JavaScript`, `localstorage`
-            ]
-        },
-        {
-            title:          `compass`,
-            description:    `A responsive demo website with Google Maps API, Bootstrap, and jQuery UI.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/compassDesktop.png`,
-            link:           `https://louisiaegerv.github.io/compass`,
-            tags:           [
-                `JavaScript`, `APIs`, `jQuery`
-            ]
-        },
-        {
-            title:          `stocks-nodejs`,
-            description:    `Get any stocks current NASDAQ price with this NodeJS app.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/blob/master/stock2.png?raw=true`,
-            link:           `https://github.com/louisiaegerv/stocks-nodejs`,
-            tags:           [
-                `JavaScript`, `NodeJS`, `APIs`
-            ]
-        },
-        {
-            title:          `weather-nodejs`,
-            description:    `Get the temp and weather for any ZIP with this NodeJS application.`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/weather2.png`,
-            link:           `https://github.com/louisiaegerv/weather-nodejs`,
-            tags:           [
-                `JavaScript`, `nodeJS`, `APIs`
-            ]
-        },
-        {
-            title:          `quote`,
-            description:    `Random quote generator made with JavaScript & Twitter API.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/quoteApp.jpg`,
-            link:           `https://louisiaegerv.github.io/quote`,
-            tags:           [
-                `JavaScript`, `APIs`
-            ]
-        },
-        {
-            title:          `resume`,
-            description:    `My responsive online resume`,
-            img:            `https://github.com/louisiaegerv/resume/raw/master/assets/img/my-resume.png`,
-            link:           `https://louisiaegerv.github.io/resume`,
-            tags:           [
-                `SASS`
-            ]
-        },
-        {
-            title:          `Zephyr layout`,
-            description:    `The responsive and mobile-first template site I created with ReactJS, HTML5, and CSS.`,
-            img:            `https://github.com/louisiaegerv/Project-Images/raw/master/ZephyrSite.jpg`,
-            link:           `https://louisiaegerv.github.io/zephyr/`,
-            tags:           [
-                `JavaScript`, `ReactJS`
-            ]
-        },
-        {
-            title:          `Cobalt layout`,
-            description:    `Another responsive website theme I created using HTML and CSS`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/cobalt.png`,
-            link:           `https://louisiaegerv.github.io/cobalt`,
-            tags:           [
-                `JavaScript`
-            ]
-        },
-        {
-            title:          `noteboard`,
-            description:    `For all those genius ideas you keep forgetting.`,
-            img:            `https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/noteboard.png`,
-            link:           `https://louisiaegerv.github.io/noteboard`,
-            tags:           [
-                `JavaScript`, `jQuery`
+                `Java`, `Android Dev` , `Firebase`
             ]
         }
+        
     ]},
     contact: {
-        email: `louis.iaeger@yahoo.com`
+        email: `imrank.sensea@gmail.com`
     },
     social: [
         {
-            name: 'twitter',
-            shortlink: 'twitter.com/theCodeslayer/',
-            fullLink: 'https://twitter.com/theCodeslayer/',
+            name: 'Instagram',
+            shortlink: 'instagram.com/syntax.404/',
+            fullLink: 'https://www.instagram.com/syntax.404/',
             img: '',
             fontAwesome: true
         },
         {
             name: 'github',
-            shortlink: 'github.com/louisiaegerv/',
-            fullLink: 'https://github.com/louisiaegerv/',
+            shortlink: 'github.com/mifune96',
+            fullLink: 'https://github.com/mifune96',
             img: '',
             fontAwesome: true
-        },
-        {
-            name:       'linkedin',
-            shortlink:  'linkedin.com/in/louislinkedin',
-            fullLink:   'https://www.linkedin.com/in/louislinkedin',
-            img:        '',
-            fontAwesome: true
-        },
-        {
-            name:       'Codecademy',
-            shortlink:  'codecademy.com/megapandalord',
-            fullLink:   'https://www.codecademy.com/megapandalord',
-            img:        'https://louisiaegerv.github.io/resume/assets/img/cc2w.png',
-            fontAwesome: false
-        },
-        {
-            name:       'Team Treehouse',
-            shortlink:  'teamtreehouse.com/louisiaeger',
-            fullLink:   'https://teamtreehouse.com/louisiaeger',
-            img:        'https://raw.githubusercontent.com/louisiaegerv/Project-Images/master/treehouse.png',
-            fontAwesome: false
         }
+       
     ]
 };
